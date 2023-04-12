de4c004f2ad491a72d34144ab57b9d03777c87b4
QuiD-0 <wodnd101@naver.com>
2023-04-12 14:57:32 +0900

    컨트롤러 생성

---
+++ src/main/kotlin/com/quid/payments/gateway/in/http/PaymentController.kt
@@ -0,0 +1,9 @@
+package com.quid.payments.gateway.`in`.http
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/payment")
+class PaymentController {
+}
