d0a56da4d0ecec4d3508579f10bd048f7742d12b
QuiD-0 <wodnd101@naver.com>
2023-04-24 10:30:44 +0900

    WebSecurityCustomizer 추가

--- src/main/kotlin/com/quid/unit/security/config/AuthenticationConfig.kt
+++ src/main/kotlin/com/quid/unit/security/config/AuthenticationConfig.kt
@@ -4,6 +4,8 @@ import com.quid.unit.security.JwtTokenFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
@@ -16,12 +18,14 @@ class AuthenticationConfig {
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain =
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
-            .authorizeHttpRequests()
-            .requestMatchers("/token/issue/**").permitAll()
-            .anyRequest().authenticated()
-            .and()
             .addFilterBefore(JwtTokenFilter(), BasicAuthenticationFilter::class.java)
             .csrf().disable()
             .build()
+
+
+    @Bean
+    fun webSecurityCustomizer(): WebSecurityCustomizer = WebSecurityCustomizer { web: WebSecurity ->
+        web.ignoring().requestMatchers("/token/issue/**", "/")
+    }
 
 }
