02ba8dbcb1ae6de293b0dd4854ba41183d5f31ae
QuiD-0 <wodnd101@naver.com>
2023-04-27 09:08:52 +0900

    enableWebSecurity

--- src/main/kotlin/com/quid/unit/security/config/AuthenticationConfig.kt
+++ src/main/kotlin/com/quid/unit/security/config/AuthenticationConfig.kt
@@ -5,20 +5,22 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
+import org.springframework.web.filter.OncePerRequestFilter
 
 
 @Configuration
+@EnableWebSecurity
 class AuthenticationConfig {
 
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain =
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
-            .addFilterBefore(JwtTokenFilter(), BasicAuthenticationFilter::class.java)
+            .addFilterBefore(JwtTokenFilter(), OncePerRequestFilter::class.java)
             .csrf().disable()
             .build()
 
